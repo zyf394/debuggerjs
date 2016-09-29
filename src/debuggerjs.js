4bc04189446c5131bb65c99320189d9e7d802f17
zhongyufei <zhongyufei@didichuxing.com>
2016-09-29 17:35:56 +0800

    optimise for mobile

--- src/debuggerjs.js
+++ src/debuggerjs.js
@@ -17,50 +17,46 @@ class DebuggerInstance {
     create() {
         let me = this;
         let error = me.error;
+        let count = Debugger.errorCount;
         let timeStamp = +new Date();
 
         let content = `
-            <div >${error.message}
-                <span debugger-event="destroy"></span>
-            </div>
+            <div >err${count}: ${error.message}</div>
             `;
         let alertBox = document.createElement('div');
         alertBox.id = 'debugger-' + timeStamp;
         alertBox.className = styles.debugger;
         alertBox.innerHTML = content;
-        alertBox.onclick = function (event) {
-            let target = event.target;
-            if (target.getAttribute('debugger-event') === 'destroy') {
-                me.destroy();
-            }
-            event.stopPropagation(); // prevent click events trigger on other elements again
-        };
 
         me.instance = alertBox;
 
         document.body.appendChild(alertBox);
+
+        Debugger.errorCount++;
+
+        setTimeout(function () {
+            me.destroy();
+        }, 5000)
     }
 
     destroy() {
         let me = this;
-        let instance = me.instance;
-        instance && instance.parentNode && instance.parentNode.removeChild(instance);
-
-        /*me.blur()
+        me.blur()
             .then(function () {
                 let instance = me.instance;
                 instance && instance.parentNode && instance.parentNode.removeChild(instance);
-            })*/
+            })
     }
 
     blur() {
-        let instance = this.instance,
+        let me = this,
+            instance = this.instance,
             timer = null;
 
         return new Promise(function (res, rej) {
             timer = setInterval(function () {
-                let opacity = instance.style.opacity;
-                if (opacity == 0) {
+                let opacity = me.getCssValue(instance, 'opacity');
+                if (opacity < 0.1) {
                     clearTimeout(timer);
                     res()
                 } else {
@@ -73,6 +69,10 @@ class DebuggerInstance {
 
     getErrorObj(arg) {
         this.error = arg;
+    }
+
+    getCssValue(target, attr) {
+        return window.getComputedStyle(target)[attr]
     }
 
     compileToCss(obj) {
@@ -88,6 +88,9 @@ class DebuggerInstance {
 }
 
 export const Debugger = {
+
+    errorCount: 1,
+
     init() {
         this.listenScriptError();
     },
@@ -99,23 +102,23 @@ export const Debugger = {
         });
     },
     log(error){
-        if(this.isEvent(error)){
+        if (this.isEvent(error)) {
             var target = error.target;
             error.message = "Resource Error: can't get " + (target.src || target.href) + ".";
             new DebuggerInstance(error);
-        }else if(this.isErrorEvent(error)){
+        } else if (this.isErrorEvent(error)) {
             new DebuggerInstance(error);
-        }else if(this.isProgressEvent(error)){
+        } else if (this.isProgressEvent(error)) {
             new DebuggerInstance(error);
-        }else if(this.isXHR(error)){
+        } else if (this.isXHR(error)) {
             error.message = "AJAX Error: XMLHttpRequest failed. Did you use $.ajax? the Debugger can't get more detail from error callback. Please check your $.ajax settings."
             new DebuggerInstance(error);
-        }else if(this.isString(error)){
+        } else if (this.isString(error)) {
             new DebuggerInstance({message: error});
-        }else if(this.isUndefined(error)){
+        } else if (this.isUndefined(error)) {
             error.message = "Params Error: Debugger.log(...) must have 1 param in it, but found none";
             new DebuggerInstance(error);
-        }else{
+        } else {
             error.message = "Unknown Error.";
             new DebuggerInstance(error);
         }
--- test/test.js
+++ test/test.js
@@ -1,13 +1,17 @@
 /**
  * Created by didi on 16/9/23.
  */
+(function (e) {
+    e.setAttribute("src", "http://172.26.11.24:1234/target/target-script-min.js#anonymous");
+    document.getElementsByTagName("body")[0].appendChild(e);
+})(document.createElement("script"));
 
 require('./reset.css');
 
 import  {Debugger} from '../src/debuggerjs'
-document.onclick = function(){
-    alert(a)
-}
+document.addEventListener('touchend',function (e) {
+    alert(a);
+});
 
 function loadErrorScript() {
     var script = document.createElement('script');
@@ -17,26 +21,26 @@ function loadErrorScript() {
     };
     document.body.appendChild(script);
 }
-function loadErrorLink(){
+function loadErrorLink() {
     var link = document.createElement('link');
     link.href = '/kkk.css';
     link.rel = 'stylesheet';
-    link.onerror = function(error){
+    link.onerror = function (error) {
         console.log(error);
         Debugger.log(error)
     };
     document.head.appendChild(link);
 }
 
-function ajax(opt){
+function ajax(opt) {
     var xhr = new XMLHttpRequest();
     xhr.open(opt.type, opt.url, opt.async);
-    xhr.onload = function(res) {
-        if(this.status == 200||this.status == 304){
+    xhr.onload = function (res) {
+        if (this.status == 200 || this.status == 304) {
             opt.success(res);
         }
     };
-    xhr.onerror = function(error) {
+    xhr.onerror = function (error) {
         opt.error(error)
     };
     xhr.send();
@@ -47,13 +51,13 @@ window.onload = function () {
     loadErrorScript();
     loadErrorLink();
     $.ajax({
-        type:'get',
-        url:'http://static.galileo.xiaojukeji.com/static/tms/api/poisearch.json',
-        dataType:'json',
-        success:function(res){
+        type: 'get',
+        url: 'http://static.galileo.xiaojukeji.com/static/tms/api/poisearch.json',
+        dataType: 'json',
+        success: function (res) {
             alert(res)
         },
-        error:function(err){
+        error: function (err) {
             Debugger.log(err)
         }
     })
